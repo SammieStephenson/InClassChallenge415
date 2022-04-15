a7f02f1ebdab4d83bf2b027a874684ba208307f5
Sammie Stephenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-04-15 13:48:23 -0400

    first commmit

---
+++ script.js
@@ -0,0 +1,13 @@
+let userInput1 = prompt("Deposit Money OR Withdraw Money?");
+
+switch (userInput1) {
+    case 'Deposit Money':
+        prompt('How much would you like to deposit?');
+        break;
+    case 'Withdraw Money':
+        prompt('How much would you like to withdraw ?')
+
+}
+
+
+
