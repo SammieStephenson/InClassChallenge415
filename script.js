db1e2f5c5de3632be4ac6230a4dbbe5c6eacdc6d
Sammie Stephenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-04-15 13:57:54 -0400

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let userInput1 = prompt("Deposit Money OR Withdraw Money?");
 
+
 switch (userInput1) {
     case 'Deposit Money':
-        prompt('How much would you like to deposit?');
+        let userInput2 = prompt('How much would you like to deposit?');
+        break;
         break;
     case 'Withdraw Money':
-        prompt('How much would you like to withdraw ?')
+        let userInput3 = prompt('How much would you like to withdraw ?');
+} return userInput3;
+return userInput2;
 
+if prompt = "How much would you like to deposit?" {
+    alert('Your balance is')
 }
-
-
 
